9b082f010362feb63c62d1956b5525a240895ac5
Hafiz Caniago <hafizcode02@gmail.com>
2025-05-15 12:13:18 +0700

    feat: add dummy dashboard

--- app/Views/pages/dashboard/index.php
+++ app/Views/pages/dashboard/index.php
@@ -26,7 +26,7 @@
             <div class="inner">
                 <h3>150</h3>
 
-                <p>New Orders</p>
+                <p>Transaksi Baru</p>
             </div>
             <div class="icon">
                 <i class="ion ion-bag"></i>
@@ -37,14 +37,29 @@
     <!-- ./col -->
     <div class="col-lg-3 col-6">
         <!-- small box -->
-        <div class="small-box bg-success">
+        <div class="small-box bg-danger">
             <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <h3>1050</h3>
 
-                <p>Bounce Rate</p>
+                <p>Jumlah Transaksi Tahun Ini</p>
             </div>
             <div class="icon">
-                <i class="ion ion-stats-bars"></i>
+                <i class="ion ion-cash"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>Rp. 1,25 jt</h3>
+
+                <p>Total Transaksi Hari Ini</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-cash"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
@@ -54,27 +69,12 @@
         <!-- small box -->
         <div class="small-box bg-warning">
             <div class="inner">
-                <h3>44</h3>
+                <h3>Rp. 53 jt</h3>
 
-                <p>User Registrations</p>
+                <p>Total Transaksi Bulan Ini</p>
             </div>
             <div class="icon">
-                <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-            <div class="inner">
-                <h3>65</h3>
-
-                <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-pie-graph"></i>
+                <i class="ion ion-cash"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
