1c305b693d90f52322fc669d7bcc35ae7e30ef12
Hafiz Caniago <hafizcode02@gmail.com>
2025-05-15 10:02:01 +0700

    feat: change load resource to min.css / min.js

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -32,7 +32,7 @@
                 <section class="content">
                     <div class="container-fluid">
                         <?= $this->renderSection('main-content') ?>
-                        
+
                     </div>
                 </section>
             </div>
@@ -50,7 +50,7 @@
     <!-- Bootstrap -->
     <script src="<?= base_url('plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
     <!-- AdminLTE -->
-    <script src="<?= base_url('dist/js/adminlte.js') ?>"></script>
+    <script src="<?= base_url('dist/js/adminlte.min.js') ?>"></script>
     <!-- SweetAlert2 -->
     <script src="<?= base_url('plugins/sweetalert2/sweetalert2.min.js') ?>"></script>
     <!-- Toastr -->
